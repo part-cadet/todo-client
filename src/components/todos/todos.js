9d1a613d9e8b6a363d212fd159871579350a3527
Galiold <ali.goldani@ymail.com>
2019-07-31 02:15:25 +0430

    clearing up

--- src/components/todos/todos.js
+++ src/components/todos/todos.js
@@ -1,17 +1,10 @@
-import {
-  TodoBoard
-} from './todoBoard/todo-board-model';
-
+import { TodoBoard } from '../../models/todos/todo-board-model';
 
 export class Todos {
   constructor() {
-    console.log('hi');
     this.todoBoards = [];
     this.todoBoards.push(new TodoBoard('hi'));
     this.todoBoards.push(new TodoBoard('hello'));
     this.todoBoards.push(new TodoBoard('hello'));
-
-
-    
   }
 }
