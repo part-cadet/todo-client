e32af5df901f579bf0ab7e55714c7c2e71fe91b3
Galiold <ali.goldani@ymail.com>
2019-07-31 02:18:04 +0430

    adding task function added to board

--- src/components/todos/todoBoard/todo-board.js
+++ src/components/todos/todoBoard/todo-board.js
@@ -1,19 +1,22 @@
 //import {Task} from './task/task'
 import {bindable} from 'aurelia-framework';
-import {Task} from './task/task-model';
+import {Task} from '../../../models/todos/task-model';
 
 
 export class TodoBoard {
-  @bindable todoboard123;
-  constructor() {
-    
- 
-    
+  @bindable todoboard;
 
+  constructor() {
+    this.newTaskDesc = '';
     this.tasks = [];
     this.tasks.push(new Task('this is a task'));
     this.tasks.push(new Task('this is a task'));
-    
+  }
 
+  addTask() {
+    if (this.newTaskDesc !== '') {
+      this.tasks.push(new Task(this.newTaskDesc));
+      this.newTaskDesc = '';
+    }
   }
 }
