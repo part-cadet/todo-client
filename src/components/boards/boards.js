86ccd528ffea55e75d646aabd6538c1ddcba6104
Galiold <ali.goldani@ymail.com>
2019-07-31 12:51:19 +0430

    adding boards function added

--- src/components/boards/boards.js
+++ src/components/boards/boards.js
@@ -1,8 +1,12 @@
-import {Board}  from './board/board-model';
+import {Board}  from '../../models/boards/board-model';
 export class Boards {
   constructor() {
     this.boards = [];
     this.boards.push(new Board('board Nafiseh', 'Nafise', ['Negin Khatibzadeh', 'Fatemeh Ghanbari']));
     this.boards.push(new Board('board Faezeh', 'Nafise', ['Negin Khatibzadeh', 'Fatemeh Ghanbari', 'Nafiseh Nikeghbal', 'Nafiseh Nikeghbal']));
+  }
+
+  addBoard() {
+    this.boards.push(new Board('New Board', 'Owner', []));
   }
 }
