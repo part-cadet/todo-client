4cd9a39bd6b1bdc292373634aeefd4ffb84a1360
Galiold <ali.goldani@ymail.com>
2019-07-31 12:51:44 +0430

    function for adding members added

---
+++ src/models/boards/board-model.js
@@ -0,0 +1,15 @@
+// import {Member}  from './member/member-model';
+export class Board {
+  owner;
+  title;
+  members=[];
+  constructor(title, owner, members) {
+    this.title = title;
+    this.owner = owner;
+    this.members = members;
+  }
+
+  addMember() {
+    this.members.push('New Member');
+  }
+}
