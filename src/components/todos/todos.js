28a69dcb7c53cc5f7d89a51d01250757bdf87e63
Galiold <ali.goldani@ymail.com>
2019-08-01 12:41:17 +0430

    todo request added

--- src/components/todos/todos.js
+++ src/components/todos/todos.js
@@ -1,29 +1,34 @@
-import {
-  TodoBoard
-} from '../../models/todos/todo-board-model';
+import { TodoBoard } from '../../models/todos/todo-board-model';
+import { inject } from 'aurelia-framework';
+import { HttpClient } from 'aurelia-fetch-client';
 
+@inject(HttpClient)
 export class Todos {
-  newTodoTitle
-
-
+  newTodoTitle = '';
+  todoBoards = [];
   show = true;
-  constructor() {
-    this.newTodoTitle = '';
-    this.todoBoards = [];
-    this.todoBoards.push(new TodoBoard('hi'));
-    this.todoBoards.push(new TodoBoard('hello'));
-    this.todoBoards.push(new TodoBoard('hello'));
+  constructor(httpClient) {
+    this.httpClient = httpClient;
+  }
+
+  attached() {
+    this.httpClient.fetch('todo')
+      .then(response => response.json())
+      .then(data => {
+        data.result.forEach(element => {
+          this.todoBoards.push(Object.assign(new TodoBoard('temp'), element));
+        });
+      });
   }
 
   addTodo() {
-
-
     if (this.newTodoTitle !== '') {
       this.todoBoards.push(new TodoBoard(this.newTodoTitle));
       this.newTodoTitle = '';
     }
     this.show = !this.show;
   }
+
   toggle() {
     this.show = !this.show;
     // console.log("show");
