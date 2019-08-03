d69acc89cb2ac398e96c13e17120cfe65d005f20
Galiold <ali.goldani@ymail.com>
2019-08-03 14:51:57 +0430

    info board model bined completely

--- src/models/InfoBoardModel.js
+++ src/models/InfoBoardModel.js
@@ -1,6 +1,30 @@
+// import { TodoBoard } from '../models/todos/todo-board-model';
+import { HttpClient } from 'aurelia-fetch-client';
+import { inject } from 'aurelia-framework';
+
+@inject(HttpClient)
 export class InfoBoardModel {
   title;
-  constructor(title) {
-    this.title = title;
+  todocount;
+  taskcount;
+  unfinishedtasks;
+  todoBoards = [];
+  constructor(httpClient) {
+    this.httpClient = httpClient;
   }
+
+  // TODO: Should fix the query to get todos for each board
+  // attached() {
+  //   this.httpClient.fetch('todos')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       this.todoBoards = data.map(element => Object.assign(new TodoBoard(), element));
+  //     });
+
+  //   this.httpClient.fetch(`boards/owner/${id}`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       this.todoBoards = data;
+  //     });
+  // }
 }
