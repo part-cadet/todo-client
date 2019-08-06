b0d5e9583aba8a00d82eaaf6810221072c6498b4
Faezeh <shayesteh.fa@gmail.com>
2019-08-06 16:07:51 +0430

    update task added

--- src/components/todos/task/task.js
+++ src/components/todos/task/task.js
@@ -1,7 +1,41 @@
 import { bindable } from 'aurelia-framework';
+import {
+  HttpClient, json
+} from 'aurelia-fetch-client';
 
+import {
+  inject
+} from 'aurelia-framework';
+
+
+@inject(HttpClient)
 export class Task {
   @bindable task
 
-  constructor() {}
+  constructor(httpClient) {
+    this.httpClient = this.httpClient;
+  }
+
+
+  updateTask(value) {
+    console.log("task id "+this.task.id);
+    
+    this.httpClient.fetch(`task/${this.task.id}`, {
+      method: 'PUT',
+      body: json({
+        description: "updated",
+        done: value
+      })
+    })
+
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
+  }
+
+
+  logchange(value) {
+    console.log("here is the value"+value);
+  }
 }
--- src/models/todos/task-model.js
+++ src/models/todos/task-model.js
@@ -1,4 +1,5 @@
 export class Task {
+  id;
   description;
   todo_id;
   done;
