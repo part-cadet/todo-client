7722a2abaac893439f6aadf729cea0dc3104d8b1
Galiold <ali.goldani@ymail.com>
2019-08-08 15:45:03 +0430

    dynamic title added and delete functions rewrote

--- src/components/todos/todos.js
+++ src/components/todos/todos.js
@@ -1,27 +1,41 @@
 import { TodoBoard } from '../../models/todos/todo-board-model';
 import { InfoBoardModel } from '../../models/InfoBoardModel';
+import { Router } from 'aurelia-router';
 import { inject } from 'aurelia-framework';
 import { HttpClient, json } from 'aurelia-fetch-client';
 
 import { ValidationControllerFactory, ValidationRules } from 'aurelia-validation';
 import { BootstrapFormRenderer } from '../bootstrap-form-renderer';
 
-@inject(HttpClient, ValidationControllerFactory)
+@inject(HttpClient, ValidationControllerFactory, Router)
 export class Todos {
   newTodoTitle = '';
   todoBoards = [];
   show = true;
   boards = [];
   displayBoardID;
+  displayBoardTitle;
+  notRedirected = false;
 
-  constructor(httpClient, controllerFactory) {
+  constructor(httpClient, controllerFactory, router) {
     this.controller = controllerFactory.createForCurrentScope();
     this.controller.addRenderer(new BootstrapFormRenderer());
     this.httpClient = httpClient;
+    this.router = router;
   }
 
   attached() {
     this.getBoards();
+  }
+
+  activate(params, routeConfig, navigationInstruction) {
+    if (params.boardID == null) {
+      console.log('not redirected')
+      this.notRedirected = true;
+    } else {
+      this.displayBoardID = params.boardID;
+      console.log(`board given is ${this.displayBoardID}`);
+    }
   }
 
   getBoards() {
@@ -29,12 +43,18 @@ export class Todos {
       .then(response => response.json())
       .then(data => {
         this.boards = data.map(element => Object.assign(new InfoBoardModel(), element));
-        this.displayBoardID = this.boards[0].id;
+        if (this.notRedirected) {
+          console.log('not redirected')
+          this.displayBoardID = this.boards[0].id;
+        }
+        const displayBoard = this.boards.find(element => element.id === parseInt(this.displayBoardID, 10) )
+        this.displayBoardTitle = displayBoard.title;
         this.getTodoBoards();
       });
   }
 
   getTodoBoards() {
+    // console.log(`board requested is ${this.displayBoardID}`);
     this.httpClient.fetch(`boards/todosof/${this.displayBoardID}`)
       .then(response => response.json())
       .then(data => {
@@ -44,7 +64,10 @@ export class Todos {
   }
 
   changeDisplayBoard(boardID) {
+    this.router.navigate(`/todos?boardID=${boardID}`);
     this.displayBoardID = boardID;
+    const displayBoard = this.boards.find(element => element.id === parseInt(this.displayBoardID, 10) )
+    this.displayBoardTitle = displayBoard.title;
     this.getTodoBoards();
   }
 
@@ -61,9 +84,9 @@ export class Todos {
             .then(response => response.json())
             .then(data => {
               console.log(data);
+              this.getTodoBoards();
             });
 
-          this.getTodoBoards();
           this.newTodoTitle = '';
           this.toggle();
         } else {
