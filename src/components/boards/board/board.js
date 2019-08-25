0cff96b007dd66b16185315a7ed3c34837f7d3b8
nafisehnikeghbal <nafise.nikeghbal32@gmail.com>
2019-08-25 14:46:05 +0430

    change update board

--- src/components/boards/board/board.js
+++ src/components/boards/board/board.js
@@ -1,12 +1,15 @@
 import {bindable, bindingMode} from 'aurelia-framework';
 //import { TodoBoard } from '../../../models/todos/todo-board-model';
-import {HttpClient} from 'aurelia-fetch-client';
+import {HttpClient, json} from 'aurelia-fetch-client';
 import { inject } from 'aurelia-framework';
 
 @inject(HttpClient)
 export class Board {
   @bindable({ defaultBindingMode: bindingMode.twoWay }) board;
   @bindable({ defaultBindingMode: bindingMode.twoWay }) refreshboards;
+  onEditMode=false;
+  editOwner;
+  editTitle;
 
   constructor(httpClient) {
     this.httpClient = httpClient;
@@ -40,6 +43,35 @@ export class Board {
       .then(data => {
         this.refreshboards();
       });
+  }
+  updateBoard() {
+    // this.controller.validate()
+    //   .then(result => {
+    //     if (result.valid) {
+    this.httpClient.fetch(`boards/${this.board.id}`, {
+      method: 'PUT',
+      body: json({
+        title: this.editTitle,
+        owner: this.editOwner
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.onEditMode = false;
+        this.refreshboards();
+      });
+    // } else {
+    //   console.log(result);
+    // }
+    // });
+  }
+  toggleEditMode(mode) {
+    if (mode === 'enter') {
+      this.editTitle = this.board.title;
+      this.editOwner = this.board.owner;
+      this.onEditMode = true;
+    }
   }
 }
 
